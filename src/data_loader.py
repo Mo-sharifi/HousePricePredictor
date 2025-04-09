db631d3ddb75d4e7282b9bd7016f5ba5f7d2c88b
mohammadreza sharifi <m.sharifi8080@gmail.com>
2025-04-09 14:03:21 +0330

    Add function to load geolocation data and fix circular import issue.

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 
-def load_data(
+def load_tehran_data(
     file_path="/home/dili/Univers/HousePricePredictor/data/kashefi_dataset.csv",
 ) -> pd.DataFrame:
     """
@@ -36,10 +36,51 @@ def load_data(
         print("Data loading process completed.")
 
 
-def main():
-    data = load_data()
+def load_geolocation_data(
+    file_path=r"/home/dili/Univers/HousePricePredictor/data/lat_long_address.csv",
+):
+    """
+    Load geolocation data from a CSV file.
 
-    print(data.head())
+    Parameters:
+    file_path (str): The path to the CSV file.
+
+    Returns:
+    pd.DataFrame: The loaded geolocation data as a pandas DataFrame.
+    """
+
+    try:
+        data = pd.read_csv(file_path)
+
+        return data
+
+    except FileNotFoundError:
+
+        print(f"Error: The file {file_path} was not found.")
+
+        return None
+
+    except pd.errors.EmptyDataError:
+        print(f"Error: The file {file_path} is empty.")
+
+        return None
+
+    finally:
+
+        print("Geolocation data loading process completed.")
+
+
+def main():
+
+    data = load_tehran_data()
+    df_geolocation = load_geolocation_data()
+
+    from preprocess import log_dataframe
+
+    if data is not None:
+        log_dataframe(data, "Data", 5)
+    if df_geolocation is not None:
+        log_dataframe(df_geolocation, "Geolocation Data", 5)
 
 
 if __name__ == "__main__":
